e0074000083b1648d119a5df6a8b980d0562debc
Gilbert Gotora <gilbertgotora2@gmail.com>
2020-09-28 10:43:42 +0200

    add get username, display board, user move

--- bin/main.rb
+++ bin/main.rb
@@ -1,10 +1,22 @@
 #!/usr/bin/env ruby
 
 # display welcome message
-puts 'Tic Tac Toe!'
+puts 'Welcome Ade\'s to Tic Tac Toe!'
 # get player names
 puts 'Please Enter Your Name '
 name = gets.chomp
+puts "Welcome #{name}"
 
-#
+# display board 
+def display_board
+  puts " 1 | 2 | 3 "
+  puts " --------- "
+  puts " 4 | 5 | 6 "
+  puts " --------- "
+  puts " 7 | 8 | 9 "
+end
+
+display_board
 # get player move
+puts "Enter your choice between 1 and 9"
+choice = gets.chomp
