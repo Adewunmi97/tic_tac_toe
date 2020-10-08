3044fe6563fbfadc8012b8bf75ff55d60ce38856
Gilbert Gotora <gilbertgotora2@gmail.com>
2020-10-08 06:51:56 +0200

    finaliz  project

--- bin/main.rb
+++ bin/main.rb
@@ -1,39 +1,35 @@
 #!/usr/bin/env ruby
+# rubocop: disable Metrics/MethodLength
 require_relative '../lib/game.rb'
 require_relative '../lib/player.rb'
 
-class Player
-  attr_accessor :name, :score, :tag
-  def initialize(name, tag)
-    @name = name
-    @tag = tag
-    @score = []
-  end
-end
+game = Game.new
+
+p game.board
 
 def space
   puts ''
 end
 
-def line
-  puts '_'
-end
-
 def welcome_message
-  line * 50
+  puts '_' * 50
   space
   puts '                      WELCOME TO THE WONDER GAME OF TIC TIC TOE'
-  line * 50
+  puts '_' * 50
   space
   puts 'Players take turns to select from 9 positions on the board below'
   puts 'First to make a horizontal or vertical line of their selection wins'
   space
 end
 
-game = Game.new(player1, player2)
-WINNING_COMBINATION = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 5], [1, 4, 6], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-
-# #gets player names
+def display_board(board)
+  space
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts '-----------'
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts '-----------'
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
 def player_names
   players = []
@@ -44,7 +40,8 @@ def player_names
       puts "player #{i + 1} please Enter Your Name"
       space
       name = gets.chomp.upcase
-      break if name.length > 0 
+      break if name.length.positive?
+
       puts 'Invalid Entry'
     end
     puts "Welcome #{name} :"
@@ -52,27 +49,25 @@ def player_names
   end
   players
 end
-def display_board(game.board)
-  space
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts '-----------'
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts '-----------'
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+
+def welcome_players(players)
+  puts ''
+  puts '_' * 50
+  puts ''
+  puts ''
+  puts "                        #{players[0].name}  vs  #{players[1].name}"
+  puts '                    Let the games begin'
+  puts '                       Select from 1-9'
+  puts '_' * 50
+  puts ''
 end
 
-players = player_names
-player1 = Player.new(players[0], 'X')
-player2 = Player.new(players[1], 'O')
-players = [player1, player2]
-puts ''
-puts '_'*50
-puts "                 #{player1.name} vs #{player2.name}"
-puts '                    Let the games begin'
-puts '_'*50
-puts ''
+def end_game
+  puts 'Thank You For Playing Tic Tac Toe'
+  puts 'Game on Vanilla Ruby Build by Gilbert Gotora & Shonibare Adewunmi Comfort'
+end
 
-def restart(game, player, board)
+def restart(game, board)
   space
   space
   space
@@ -81,84 +76,54 @@ def restart(game, player, board)
   loop do
     option = gets.chomp.upcase
     if option == 'Y'
-      move(game, player, board)
+      board[0] = 1
+      board[1] = 2
+      board[2] = 3
+      board[3] = 4
+      board[4] = 5
+      board[5] = 6
+      board[6] = 7
+      board[7] = 8
+      board[8] = 9
+      play(game, board)
     else
-      break
-    end
-  end
-
-
-def winner?(player, board)
-  tot = []
-  WINNING_COMBINATION.each do |comb|
-    score = board.each_index.select{|i| board[i] == player.tag}
-    tot = score & comb
-    
-  end
-  return true if tot.length == 3
-end
-
-def restart(player, board)
-
-  space
-  space
-  space
-  puts 'Do You Want To Play Again?'
-  puts 'Enter Y or N'
-  loop do
-    option = gets.chomp.upcase
-    if option == 'Y'
-      board[0] = 1; board[1] = 2
-      board[2] = 3; board[3] = 4
-      board[4] = 5; board[5] = 6
-      board[6] = 7; board[7] = 8; board[8] = 9
-      move(player, board)
-    else
-      return 
+      end_game
     end
   end
 end
 
-def move(player, board)
-  puts 'Enter move between 1 - 9'
-  puts "#{player.name} It is  your turn"
-  input = game.player_input
-  if game.valid?(board, input)
-    board[input] = player.tag
-
-    display_board(game.board)
-    puts "#{player.name} WON!!" if winner?(player, board)
-    restart(player, board) if winner?(player, board)
-    # player.score << input
-  elsif game.position_taken?(board, input)
-    puts "Position #{board[input]} taken - Try Again"
-    move(game, player, board)
-  else
-    puts 'Invalid Entry - Try Again'
-    move(game, player, board)
-
-  end
-  return player.score
-end
-
-def play(game, players, board)
-  count = 0
-  while count < 9
+def move(game, players, board)
+  9.times do |x|
     2.times do |i|
-      players[i].score = move(players[i], board)
-
-      # if winner?(players[i].score)
-      #   puts "DBG Winner Score #{winner?(players[i].score)}"
-      #   puts "#{players[i].name} HAS WON!!"
-      #   return "#{players[i].name} HAS WON!!"
-      # else
-      #   # puts "SCore = #{players[i].score}"
-      # end
-
+      input = game.player_input
+      if game.valid?(board, input)
+        board[input] = players[i].tag
+        display_board(board)
+        puts "#{players[i].name} WON!!" if game.winner?(players[i], board)
+        restart(game, board) if game.winner?(players[i], board)
+      elsif game.position_taken?(board, input)
+        puts "Position #{board[input]} taken - Try Again"
+        move(game, players, board)
+      else
+        puts 'Invalid Entry - Try Again'
+        move(game, players, board)
+      end
     end
-    puts count 
-    count += 1
+    restart(game, board) if x == 9
   end
 end
 
-play(game, players, board)
+def play(game, board)
+  display_board(board)
+  players = player_names
+  player1 = Player.new(players[0], 'X')
+  player2 = Player.new(players[1], 'O')
+  players = [player1, player2]
+  welcome_players(players)
+  display_board(board)
+  move(game, players, board)
+end
+
+play(game, game.board)
+
+# rubocop:enable Metrics/MethodLength
--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,35 @@
-require_relative './board.rb'
 class Game
-    attr_accessor :board
-    attr_accessor :player1, :player2
-    def initialize()
-        @board = Board.new
-        @player1 = nil
-        @player2 = nil
+  attr_accessor :player1, :player2, :board, :winning_combination
+  def initialize()
+    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @winning_combination = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    @player1 = nil
+    @player2 = nil
+  end
+
+  def player_input
+    puts 'Select Spot 1 - 9'
+    gets.chomp.to_i - 1
+  end
+
+  def int_between_1_and_9?(input)
+    input.is_a?(Integer) && input.between?(0, 8)
+  end
+
+  def position_taken?(board, input)
+    board[input] == 'X' || board[input] == 'O'
+  end
+
+  def valid?(board, input)
+    !position_taken?(board, input) && int_between_1_and_9?(input)
+  end
+
+  def winner?(player, board)
+    tot = []
+    @winning_combination.each do |combination|
+      score = board.each_index.select { |i| board[i] == player.tag }
+      tot = score & combination
     end
+    return true if tot.length == 3
+  end
 end
